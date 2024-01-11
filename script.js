4fd3555d87d7b1c928ef11840a6d0fe1b0b5da96
jakedrifter <142671011+jakedrifter@users.noreply.github.com>
2024-01-10 23:30:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function line(x1a, y1a, x2a, y2a) {
+  let side = 0;
+  let x1 = x1a,y1 = y1a,x2 = x2a, y2 = y2a;
+  let dx = Math.abs(x2a - x1a);
+  let dy = Math.abs(y2a - y1a);
+  if (dx <= dy) {
+    side = 1;
+    x1 = y1a;
+    y1 = x1a;
+    x2 = y2a;
+    y2 = x2a;
+    dx = Math.abs(y2a - y1a);
+    dy = Math.abs(x2a - x1a);
+  }
+  let pk = 2 * dy - dx;
+  for (let i = 0; i <= dx; i++) {
+    if (!side) {
+      // plot as x1, y1
+    } else {
+      // plot as y1, x1
+    }
+    if (x1 < x2) x1++; else x1--;
+    if (pk < 0) pk = pk + 2 * dy;
+    else {
+      if (y1 < y2) y1++; else y1--;
+      pk = pk + 2 * dy - 2 * dx;
+    }
+  }
+}
